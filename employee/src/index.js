9ff375aaeed13e8d5ef67043d99357ada3b56751
Jyoshika Gunduboina <jyoshikagunduboina@gmail.com>
2022-02-10 23:01:08 +0530

    employe details

---
+++ employee/src/index.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import Arrow from './Details';
+
+
+const App=()=> {
+  return (
+    <>
+    <center>
+    <Arrow /> 
+    </center> 
+    </>
+  )
+}
+ReactDOM.render(
+  <App />,
+  document.getElementById("root")
+);
+
